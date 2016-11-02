095ac6f4c2d84d25d98fd6f62fd8a965f60cd197
jurre <jurrestender+github@gmail.com>
2016-11-02 13:14:25 +0100

    Update to new Swift syntax and recommended build settings

--- BPM/ViewController.swift
+++ BPM/ViewController.swift
@@ -14,15 +14,15 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     @IBOutlet weak var beatCountLabel: UILabel!
     @IBOutlet weak var bpmPreciseLabel: UILabel!
 
-    var firstPressTime: NSDate!
-    var lastPressTime: NSDate!
-    var pressCount = 0
-    var timer = NSTimer()
+    var firstPressTime: Date!
+    var lastPressTime: Date!
+    var beatCount = 0
+    var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("handleTap:"))
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap(_:)))
         gestureRecognizer.delegate = self;
         background.addGestureRecognizer(gestureRecognizer)
         resetTimer()
@@ -32,9 +32,9 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         super.didReceiveMemoryWarning()
     }
 
-    func handleTap(recognizer: UITapGestureRecognizer) {
-        pressCount += 1
-        let now = NSDate()
+    func handleTap(_ recognizer: UITapGestureRecognizer) {
+        beatCount += 1
+        let now = Date()
         resetTimer()
         if firstPressTime != nil {
             lastPressTime = now
@@ -44,38 +44,38 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         }
 
         let totalDifference = (lastPressTime.timeIntervalSince1970 - firstPressTime.timeIntervalSince1970)
-        let bpm = Double(pressCount - 1) / (totalDifference / 60.0)
+        let bpm = Double(beatCount - 1) / (totalDifference / 60.0)
 
         updateView(bpm)
     }
 
     func reset() {
-        pressCount = 0
+        beatCount = 0
         firstPressTime = nil
         lastPressTime = nil
     }
 
-    @IBAction func resetButtonTapped(sender: AnyObject) {
+    @IBAction func resetButtonTapped(_ sender: AnyObject) {
         reset()
         updateView(0)
     }
 
     func resetTimer() {
         timer.invalidate()
-        timer = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "reset", userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(ViewController.reset), userInfo: nil, repeats: true)
     }
 
-    func updateView(bpm: Double) {
+    func updateView(_ bpm: Double) {
         var bpmValue = "-"
         var bpmPreciseValue = "-"
         
-        if bpm > 0 {
+        if bpm > 0 && beatCount > 4 {
             bpmValue = String(format: "%.0f", bpm)
             bpmPreciseValue = String(format: "%.2f", bpm)
         }
         
         bpmLabel.text = bpmValue
         bpmPreciseLabel.text = bpmPreciseValue
-        beatCountLabel.text = "\(pressCount) beats"
+        beatCountLabel.text = "\(beatCount) beats"
     }
 }
